9d6ec94764c4d36632e7fe2f3009186ef8972dea
MahdiSaadatiOrg <108065298+MahdiSaadatiOrg@users.noreply.github.com>
2022-08-04 11:24:22 +0430

    Add files via upload

---
+++ Double_Ended_Queue/Double_Ended_Queue_Array.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_Ended_Queue
+{
+    public class Double_Ended_Queue_Array
+    {
+        int[] array;
+        int rear = -1;
+        int front = -1;
+        public Double_Ended_Queue_Array(int len)
+        {
+            array = new int[len];
+            rear = array.Length;
+        }
+
+        private bool IsEmpty()
+        {
+            if (front == -1 && rear == array.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IsFull()
+        {
+            if ((rear + 1 == front) || rear == 0 || (front + 1 == rear))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void DeQueueFront()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty !!!");
+            }
+            else if (front == -1)
+            {
+                Console.WriteLine("The front has reached the end of the line");
+            }
+            else
+            {
+                Console.WriteLine(array[front]);
+                front--;
+            }
+        }
+
+        public void EnQueueRear(int NewNumber)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("Full !!!");
+            }
+            else
+            {
+                rear--;
+                array[rear] = NewNumber;
+            }
+        }
+
+        public void DeQueueRear()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty !!!");
+            }
+            else if (rear == array.Length)
+            {
+                Console.WriteLine("The rear has reached the end of the line");
+            }
+            else
+            {
+                Console.WriteLine(array[rear]);
+                rear++;
+            }
+        }
+
+        public void EnQueueFront(int NewNumber)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("FULL !!!");
+            }
+            else
+            {
+                front++;
+                array[front] = NewNumber;
+            }
+        }
+
+        public void PrintFront() 
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty !!!");
+            }
+            else if (front == -1)
+            {
+                Console.WriteLine("The front has reached the end of the line");
+            }
+            else
+            {
+                for (int i = 0; i <= front; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+
+        public void PrintRear()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Empty !!!");
+            }
+            else if (rear == array.Length)
+            {
+                Console.WriteLine("The rear has reached the end of the line");
+            }
+            else
+            {
+                for (int i = rear; i < array.Length; i++)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Double_Ended_Queue/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Double_Ended_Queue;
+
+Console.WriteLine("Hello, World!");
+
+Double_Ended_Queue_Array a = new(10);
+
+a.EnQueueRear(1);
+a.EnQueueRear(2);
+a.EnQueueRear(3);
+
+a.EnQueueFront(4);
+a.EnQueueFront(5);
+a.EnQueueFront(6);
+
+a.DeQueueFront();
+a.DeQueueFront();
+a.DeQueueFront();
+a.DeQueueFront();//error
+a.DeQueueFront();//error
+a.EnQueueFront(7);
+a.DeQueueFront();
+a.DeQueueFront();//error
+a.DeQueueFront();//error
+a.DeQueueFront();//error
+a.DeQueueFront();//error
+
+a.DeQueueRear();
+a.DeQueueRear();
+a.DeQueueRear();
+a.EnQueueRear(8);
+a.DeQueueRear();
+a.DeQueueRear();//error
+a.DeQueueRear();//error
